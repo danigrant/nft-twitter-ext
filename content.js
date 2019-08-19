335fcb224553a1237bcd24c3dbd80605d8803ebe
dani <mayor@dani.town>
2019-08-18 20:12:19 -0400

    init

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 void async function main() {
-  await new Promise(cb => setTimeout(cb, 2000))
+  await new Promise(cb => setTimeout(cb, 1000))
 
   let res = await getNFTs('0x8d3e809Fbd258083a5Ba004a527159Da535c8abA')
 
@@ -10,14 +10,18 @@ void async function main() {
 }()
 
 async function getNFTs(owner) {
-  let res = await fetch(`https://14ec30c9.ngrok.io/nfts/${owner}`)
+  let res = await fetch(`https://f6b38d97.ngrok.io/nfts/${owner}`)
   let json = await res.json()
   let assets = json["assets"]
   return assets
 }
 
 async function generateHTML(assets) {
-  let html = assets.map((asset) => `<div class='asset-outer' style='background-color: #${asset.background_color}'><div class='asset-inner'><img src='${asset.image_thumbnail_url}'/></div></div>` ) // alt='${asset.asset_contract.name} #${asset.token_id}'
+  let filteredAssets = assets.filter(function(asset, index, arr){
+    return asset.image_thumbnail_url != null;
+  });
+
+  let html = filteredAssets.map((asset) => `<div class='asset-outer' style='background-color: #${asset.background_color}'><a href="${asset.permalink}" target="_blank"><div class='asset-inner'><img src='${asset.image_thumbnail_url}'/></div></a></div>` ) // alt='${asset.asset_contract.name} #${asset.token_id}'
 
   return html.join('')
 }
